03cd3eb1b393cf396e483aea5e21b7a444c4191c
Anmol Sethi <hi@nhooyr.io>
2023-02-16 07:59:33 -0800

    2023-02-16 07:59:33am

--- pres/ci/game-loop.sh
+++ pres/ci/game-loop.sh
@@ -2,5 +2,7 @@
 set -eu
 
 cd -- "$(dirname "$0")/.."
-d2 --theme 1 --sketch "$@" game-loop.d2
 d2 --theme 1 --sketch "$@" game-loop.d2 game-loop.png
+if [ "$#" -eq 0 ]; then
+  d2 --theme 1 --sketch "$@" game-loop.d2 game-loop.png
+fi
