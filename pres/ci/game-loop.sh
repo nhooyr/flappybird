1800a336f3175e2959ed74c55b09bf24dcad929c
Anmol Sethi <hi@nhooyr.io>
2023-02-24 03:45:54 -0800

    2023-02-24 03:45:54am

--- pres/ci/game-loop.sh
+++ pres/ci/game-loop.sh
@@ -2,7 +2,7 @@
 set -eu
 
 cd -- "$(dirname "$0")/.."
-d2 --theme 1 --sketch "$@" game-loop.d2 game-loop.svg
+d2 --theme 1 --dark-theme 200 --sketch "$@" game-loop.d2 game-loop.svg
 if [ "$#" -eq 0 ]; then
-  d2 --theme 1 --sketch "$@" game-loop.d2 game-loop.png
+  d2 --theme 1 --dark-theme 200 --sketch "$@" game-loop.d2 game-loop.png
 fi
